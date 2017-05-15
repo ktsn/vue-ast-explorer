e837034dc1d7b24165c96373ba97e719651303a9
ktsn <ktsn55@gmail.com>
2017-05-15 23:35:21 +0900

    Style property value

--- src/Ast/AstNode.vue
+++ src/Ast/AstNode.vue
@@ -3,7 +3,11 @@ ast-array(v-if="isArray(value)", :value="value", :name="name", :context="context
 ast-object(v-else-if="isObject(value)", :value="value", :name="name", :context="context")
 div(v-else)
   span(v-if="name").ast-property-name {{ name }}:
-  span.ast-property-value {{ strValue }}
+  span.ast-property-value.undefined(v-if="value === undefined") undefined
+  span.ast-property-value.null(v-if="value === null") null
+  span.ast-property-value.number(v-if="typeof value === 'number'") {{ JSON.stringify(value) }}
+  span.ast-property-value.string(v-if="typeof value === 'string'") {{ JSON.stringify(value) }}
+  span.ast-property-value.boolean(v-if="typeof value === 'boolean'") {{ JSON.stringify(value) }}
 </template>
 
 <script lang="ts">
@@ -20,13 +24,6 @@ export default class AstNode extends Vue {
   })
   context: { depth: number }
 
-  get strValue(): string {
-    if (this.value === undefined) return 'undefined'
-    if (this.value === null) return 'null'
-    if (typeof this.value === 'object') return ''
-    return JSON.stringify(this.value)
-  }
-
   isObject(item: any): boolean {
     return typeof item === 'object' && item !== null
   }
@@ -41,8 +38,27 @@ export default class AstNode extends Vue {
 .ast {
   &-property-name {
     font-weight: bold;
-    color: #4c8f3d;
+    color: #3d608e;
     margin-right: 0.3em;
+  }
+
+  &-property-value {
+    &.undefined,
+    &.null {
+      color: #aaa;
+    }
+
+    &.string {
+      color: #049104;
+    }
+
+    &.number {
+      color: #18c98a;
+    }
+
+    &.boolean {
+      color: #e3990e;
+    }
   }
 }
 </style>
