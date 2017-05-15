1b65fb1a5f57cc2d03026d320a9e73d2efe8d858
ktsn <ktsn55@gmail.com>
2017-05-15 23:44:14 +0900

    Remove unused package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import Codemirror from 'vue-codemirror'
-import JsonTreeVue from 'vue-json-tree-view'
 import App from './App.vue'
 
 Vue.use(Codemirror)
-Vue.use(JsonTreeVue)
 
 new Vue({
   el: '#app',
